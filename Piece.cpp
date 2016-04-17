938c8c9e6cf8b477d3edb0f8854a5ae9d46b3118
Tran <vinh.v.tran@ucdenver.edu>
2016-04-17 09:46:55 -0600

    done Piece

---
+++ Piece.cpp
@@ -0,0 +1,87 @@
+//
+// Created by Vinh Van Tran on 4/17/16.
+//
+/*
+ //
+// Created by Ivo Georgiev on 11/22/15.
+//
+
+#ifndef PA5GAME_GAMEUNIT_H
+#define PA5GAME_GAMEUNIT_H
+
+#include <string>
+
+#include "Game.h"
+
+namespace Gaming {
+
+    class Resource;
+
+    class Piece {
+
+    private:
+        static unsigned int __idGen;
+
+        bool __finished;
+        bool __turned;
+
+        Position __position;
+
+    protected:
+        const Game &__game; // note: a reference to the Game object
+        unsigned int __id;
+
+        virtual void print(std::ostream &os) const = 0;
+
+        void finish() { __finished = true; }
+        bool isFinished() const { return __finished; }
+
+    public:
+        Piece(const Game &g, const Position &p);
+        virtual ~Piece();
+
+        const Position getPosition() const { return __position; }
+        void setPosition(const Position &p) { __position = p; }
+
+        bool getTurned() const { return __turned; }
+        void setTurned(bool turned) { __turned = turned; }
+
+        virtual void age() = 0;
+        virtual bool isViable() const = 0;
+        virtual PieceType getType() const = 0;
+
+        virtual ActionType takeTurn(const Surroundings &surr) const = 0; // note: doesn't actually change the object
+
+        virtual Piece &operator*(Piece &other) = 0;
+        virtual Piece &interact(Agent *) = 0;
+        virtual Piece &interact(Resource *) = 0;
+
+        friend std::ostream &operator<<(std::ostream &os, const Piece &piece);
+    };
+}
+
+
+#endif //PA5GAME_GAMEUNIT_H
+
+ */
+
+#include"Piece.h"
+
+namespace Gaming {
+    unsigned int Piece::__idGen = 1000;
+
+    Piece::Piece(const Game &g, const Position &p) :
+            __game(g),
+            __position(p),
+            __finished(false),
+            __turned(false),
+            __id(Piece::__idGen++) { }
+
+    Piece::~Piece() { }
+
+    std::ostream &operator<<(std::ostream &os, const Piece &piece) {
+        piece.print(os);
+        return os;
+    }
+}
+
